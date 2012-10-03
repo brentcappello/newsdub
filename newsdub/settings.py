fcfdd2ce14d3489ffbe199858e9d1958c3455f20
Brent <brentcappello@brent-laptop-linux.(none)>
2012-10-02 17:38:02 -0700

    yuppers

--- newsdub/settings.py
+++ newsdub/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'newsdubz_db',                      # Or path to database file if using sqlite3.
+        'NAME': 'newsdub_db',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'allday88',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
